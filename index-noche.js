5581ba9cc2bfc70790c713192852d152e1470198
Martin <pacholitanopacholito@gmail.com>
2025-08-22 14:39:23 -0300

    cambie el index por mi trabajo final y arregle errores con enlaces

---
+++ index-noche.js
@@ -0,0 +1,33 @@
+const divparatemperatura = document.getElementById("temperatura")
+const noche = document.getElementById("noche")
+const eliminador = document.getElementById("eliminar")
+
+fetch('https://api.openweathermap.org/data/2.5/weather?q=Dallas,US&appid=092d0bdfb66c511f17742250733ffd1c&units=metric')
+
+.then(response => {
+    if (!response.ok) throw new Error('Error');
+    return response.json();
+})
+.then(data => {
+    console.log(data);
+    
+    const temperatura = data.main.temp;
+    
+    divparatemperatura.innerHTML = `
+        <img class="tempDiv__img img--termometro" src="termometronoche.png">
+        <p class="temperatura">
+        ${temperatura}°C
+        <p>
+    `
+})
+.catch(error => {
+    console.error("Error al obtener el clima:", error);
+});
+
+noche.addEventListener("click",()=>{
+    window.location.href = "trabajoFinal.html";
+})
+
+eliminador.addEventListener("click",()=>{
+    eliminador.remove();
+})
